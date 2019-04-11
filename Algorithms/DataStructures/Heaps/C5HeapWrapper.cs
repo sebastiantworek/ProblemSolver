f1bb7b28810c9434bd0620061f47b323de0f1ec8
SEBASTIAN-PC\Sebastian <sbntworek@gmail.com>
2019-04-11 23:40:45 +0200

    Definition of Heap Interface (in progress, not incorporated) Implementation of Heap Wrapper for C5 Interval Heap

---
+++ Algorithms/DataStructures/Heaps/C5HeapWrapper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using C5;
+
+namespace Algorithms.DataStructures.Heaps
+{
+    public class C5HeapWrapper<TItem> : IHeap<TItem, C5.IPriorityQueueHandle<TItem>>
+    {
+        protected readonly C5.IntervalHeap<TItem> _intervalHeap;
+        public C5HeapWrapper(IComparer<TItem> comparer)
+        {
+            _intervalHeap = new IntervalHeap<TItem>(comparer);
+        }
+
+        public IPriorityQueueHandle<TItem> Add(TItem element)
+        {
+            IPriorityQueueHandle<TItem> handle = null;
+            _intervalHeap.Add(ref handle, element);
+            return handle;
+        }
+
+        public void Delete(IPriorityQueueHandle<TItem> handle)
+        {
+            _intervalHeap.Delete(handle);
+        }
+
+        public TItem FindMin(out IPriorityQueueHandle<TItem> handle)
+        {
+            return _intervalHeap.FindMin(out handle);
+        }
+
+        public TItem Get(IPriorityQueueHandle<TItem> handle)
+        {
+            TItem element;
+            if (!_intervalHeap.Find(handle, out element))
+                throw new IndexOutOfRangeException();
+            return element;
+        }
+
+        public void Replace(IPriorityQueueHandle<TItem> handle, TItem item)
+        {
+            _intervalHeap.Replace(handle, item);
+        }
+    }
+}
---
+++ Algorithms/DataStructures/Heaps/IHeap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.DataStructures.Heaps
+{
+    public interface IHeap<TElement, THandle>
+    {
+        TElement FindMin(out THandle handle);
+
+        THandle Add(TElement element);
+
+        void Delete(THandle handle);
+
+        void Replace(THandle handle, TElement element);
+
+        TElement Get(THandle handle);
+    }
+}
